5a8993e7b2752f638d90fe592db82dd4cad80be4
023-dev <rhdgmltkd88@gmail.com>
2026-08-05 15:55:20 +0900

    feat: Swagger 문서 계약과 설정 추가

---
+++ src/main/java/com/example/demo/common/config/api/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.example.demo.common.config.api;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Demo API")
+                        .version("1.0.0"));
+    }
+
+    @Bean
+    public GroupedOpenApi generalApi() {
+        return GroupedOpenApi.builder()
+                .group("general")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+}
