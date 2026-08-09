3034592f58dbd02b6c3fa10c5ea076292eaab6c4
023-dev <rhdgmltkd88@gmail.com>
2026-08-09 22:02:50 +0900

    refactor: api 인프라 설정 통합

---
+++ api/src/test/java/com/example/demo/common/config/PersistenceConfigTest.java
@@ -0,0 +1,22 @@
+package com.example.demo.common.config;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.data.auditing.AuditingHandler;
+
+@SpringBootTest
+class PersistenceConfigTest {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Test
+    void JPA_감사와_Redis_문자열_template을_등록한다() {
+        assertThat(applicationContext.getBeansOfType(AuditingHandler.class)).isNotEmpty();
+        assertThat(applicationContext.getBean("stringRedisTemplate")).isNotNull();
+    }
+}
