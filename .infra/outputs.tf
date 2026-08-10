a8ee72ae8c5229e79635194bd7d68e553ac455a4
023-dev <rhdgmltkd88@gmail.com>
2026-08-11 01:56:49 +0900

    Revert "Merge pull request #46 from YAPP-Github/chore/45-ec2-ssm-deploy"
    This reverts commit 90abc9ff7ec90258fd687c9382eba0f440a5691e, reversing
    changes made to ba532b9dc480aec883aa6a57878408cbb5b15f8a.

--- .infra/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "github_deploy_role_arn" {
-  description = "Set this value as the AWS_DEPLOY_ROLE_ARN repository variable."
-  value       = aws_iam_role.github_deploy.arn
-}
-
-output "ssm_document_name" {
-  description = "SSM document invoked by the CD workflow."
-  value       = aws_ssm_document.backend_deploy.name
-}
