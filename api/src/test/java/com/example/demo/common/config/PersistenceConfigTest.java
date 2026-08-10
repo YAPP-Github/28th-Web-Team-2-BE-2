f5398e0d459c500492dba7036049f815dfed8aef
023-dev <rhdgmltkd88@gmail.com>
2026-08-10 11:32:38 +0900

    fix: 리뷰 의견 반영

--- api/src/test/java/com/example/demo/common/config/PersistenceConfigTest.java
+++ api/src/test/java/com/example/demo/common/config/PersistenceConfigTest.java
@@ -2,6 +2,7 @@ package com.example.demo.common.config;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,10 +10,10 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.data.auditing.AuditingHandler;
 
 @SpringBootTest
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
 class PersistenceConfigTest {
 
-    @Autowired
-    private ApplicationContext applicationContext;
+    private final ApplicationContext applicationContext;
 
     @Test
     void JPA_감사와_Redis_문자열_template과_Querydsl_factory를_등록한다() {
