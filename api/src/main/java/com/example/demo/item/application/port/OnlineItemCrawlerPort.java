7239847b29a1b5715531f5977a17e72afc4a1347
kcw9609 <kang960953@gmail.com>
2026-08-13 01:41:55 +0900

    refactor: 미사용 HTML 크롤러 계약 제거

--- api/src/main/java/com/example/demo/item/application/port/OnlineItemCrawlerPort.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.item.application.port;
-
-import com.example.demo.item.application.command.CrawlOnlineItemCommand;
-import com.example.demo.item.application.result.CrawlOnlineItemResult;
-
-public interface OnlineItemCrawlerPort {
-
-    CrawlOnlineItemResult crawl(CrawlOnlineItemCommand command);
-}
--- api/src/main/java/com/example/demo/item/application/result/OnlineItemCrawlStatus.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.item.application.result;
-
-public enum OnlineItemCrawlStatus {
-    SUCCESS,
-    TEMPORARY_FAILURE
-}
